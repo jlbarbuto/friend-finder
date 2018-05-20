b7c0201418c03831859175053f9554da788842fa
unknown <jlbarbut@ncsu.edu>
2018-05-20 19:57:13 -0400

    functional modal!

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var surveyData = [
     {
-        friendName: "Nancy",
-        friendPhoto: "imagelink",
+        friendName: "Kiwi Bird",
+        friendPhoto: "https://phys.org/news/2015-07-kiwi-bird-genome-sequenced.html",
         scores: [
             "1",
             "1",
@@ -17,8 +17,25 @@ var surveyData = [
 
     },
     {
-        friendName: "Racel",
-        friendPhoto: "imagelink",
+        friendName: "Anteater",
+        friendPhoto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3LzFyPyrHU8F6DWbQDdNel9V7-odjlT_mXbMpsZVEPjhqo67J",
+        scores: [
+            "2",
+            "5",
+            "2",
+            "1",
+            "4",
+            "3",
+            "3",
+            "5",
+            "3",
+            "2"
+        ]
+
+    },
+    {
+        friendName: "Cuttlefish",
+        friendPhoto: "http://ilovestickyrice.com/blog/2015/11/10/you-cant-fool-a-cuttlefish/",
         scores: [
             "2",
             "5",
