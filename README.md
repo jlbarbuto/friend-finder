b4fbe7e4b073a85925766342480cc44492fe6387
unknown <jlbarbut@ncsu.edu>
2018-05-21 10:03:23 -0400

    Friends added and full functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # friend-finder
+
+## Overview
+
+This application allows users to find their new best friend! 
+
+Each user submits a form that includes 10 basic personality questions. The app compares the results to other friends already in the system and finds the closest match. Results are provided instantly after the form is submmited and provides the user with a nice picture of their newest friend!
+
+## Other information
+
+This app was built using Express for the back end as well as html and bootstrap for the front end. 
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var surveyData = [
     {
         friendName: "Kiwi Bird",
-        friendPhoto: "https://phys.org/news/2015-07-kiwi-bird-genome-sequenced.html",
+        friendPhoto: "https://activeadventures.com/assets/images/content/New_Zealand/Kiwi-bird-page.jpg",
         scores: [
             "1",
+            "5",
             "1",
+            "3",
             "5",
             "4",
-            "3",
-            "3",
             "1",
-            "2",
+            "1",
             "5",
-            "1"
+            "3"
         ]
 
     },
@@ -20,33 +20,67 @@ var surveyData = [
         friendName: "Anteater",
         friendPhoto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3LzFyPyrHU8F6DWbQDdNel9V7-odjlT_mXbMpsZVEPjhqo67J",
         scores: [
-            "2",
             "5",
-            "2",
-            "1",
             "4",
-            "3",
-            "3",
+            "1",
+            "1",
             "5",
-            "3",
-            "2"
+            "2",
+            "2",
+            "5",
+            "2",
+            "4"
         ]
 
     },
     {
         friendName: "Cuttlefish",
-        friendPhoto: "http://ilovestickyrice.com/blog/2015/11/10/you-cant-fool-a-cuttlefish/",
+        friendPhoto: "http://ilovestickyrice.com/wordpress/wp-content/uploads/2015/11/giant-cuttlefish.jpg",
         scores: [
+            "2",
+            "3",
+            "5",
+            "1",
             "2",
             "5",
             "2",
             "1",
-            "4",
-            "3",
-            "3",
+            "1",
+            "3"
+        ]
+
+    },
+    {
+        friendName: "Komodo Dragon",
+        friendPhoto: "https://icdn2.digitaltrends.com/image/komodo-dragon-720x720.jpg?ver=1.jpg",
+        scores: [
             "5",
-            "3",
-            "2"
+            "1",
+            "4",
+            "1",
+            "1",
+            "2",
+            "5",
+            "5",
+            "2",
+            "3"
+        ]
+
+    },
+    {
+        friendName: "Dragon Fly",
+        friendPhoto: "https://pixfeeds.com/images/insects/dragonflies/1280-545111084-seaside-dragonlet-dragonfly.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "2",
+            "5",
+            "1",
+            "1",
+            "5"
         ]
 
     }
